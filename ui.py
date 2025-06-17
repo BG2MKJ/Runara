e07f12474c5f3d62a1db8999df7648e51e6864da
BG2MKJ <lhw15136345190@163.com>
2025-06-17 11:20:31 +0800

    add ui

---
+++ ui.py
@@ -0,0 +1,23 @@
+import tkinter as tk
+
+class UI:
+    def confirm_button_click():
+        pass
+
+
+    def __init__(self):
+        self.root = tk.Tk()
+        self.root.title("Runara")
+        self.label_info = tk.Label(self.root,text="Runara")
+        self.root.geometry("500x500")
+        self.label_info.pack()
+        self.question_text = tk.Text(self.root)
+        self.question_text.place(x=10,y=50,width=480,height=380)
+        self.confirm_button = tk.Button(self.root,text="confirm",command=self.confirm_button_click)
+        self.confirm_button.place(x=220,y=440,width=80,height=30)
+
+
+        self.root.mainloop()
+
+
+u = UI()
